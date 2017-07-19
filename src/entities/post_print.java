51998c295ce6194dd1c1c7a3394e797600828aa6
tarowookjin <tarowj05@gmail.com>
2017-07-19 05:35:14 -0500

    Fail

--- src/entities/post_print.java
+++ src/entities/post_print.java
@@ -5,10 +5,11 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class post_print {
-	
 	DefaultTableModel model = new DefaultTableModel();
 	JTable table = new JTable(model);
-	int i=0,j=0;
+	solveINT sol =new solveINT();
+	int i=0,j=0,cont=0;
+	
 	public post_print(int cant) {
 		if(cant!=0) {
 		model.addColumn("Programa");
@@ -19,11 +20,6 @@ public class post_print {
 			}
 		}while(i!=16);//imprime los encabezados
 		
-		do{
-			if(interrupciones.impresion[j]!=null) {
-				
-			}
-		}while(j!=16);//imprime los encabezados
 		
 		}
 		else {
@@ -31,54 +27,4 @@ public class post_print {
 		}
 	}
 	
-	public void impresiones(int cant, String IRQ) {
-		if(cant==1) {
-			
-		}
-		if(cant==2) {
-			
-		}
-		if(cant==3) {
-			
-		}
-		if(cant==4) {
-			
-		}
-		if(cant==5) {
-			
-		}
-		if(cant==6) {
-			
-		}
-		if(cant==7) {
-			
-		}
-		if(cant==8) {
-			
-		}
-		if(cant==9) {
-			
-		}
-		if(cant==10) {
-			
-		}
-		if(cant==11) {
-			
-		}
-		if(cant==12) {
-			
-		}
-		if(cant==13) {
-			
-		}
-		if(cant==14) {
-			
-		}
-		if(cant==15) {
-			
-		}
-		if(cant==16) {
-			
-		}
-	}
 }
