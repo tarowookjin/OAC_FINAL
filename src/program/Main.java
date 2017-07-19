260805b94735adea1c6f6d78ff3eae46f02141aa
tarowookjin <tarowj05@gmail.com>
2017-07-18 19:29:02 -0500

    Arreglando el Main

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,5 +1,6 @@
 package program;
 
+import java.util.EmptyStackException;
 import javax.swing.*;
 
 public class Main {
@@ -7,10 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//hola
-		int ele;
+		int ele=0;
 		do{
+			try {
+			ele=0;
 			String tempele =JOptionPane.showInputDialog(null,"\t\t\t\tInterrupciones Programadas\n\t\t\t\n1-Ver integrantes del grupo\n2-Ver Tabla\n3- Agregar Interrupcion\n4-Eliminar Interrupcion\n5-Iniciar\n6-Salir");
-			 ele = Integer.parseInt(tempele);
+			if(tempele==null)
+			{throw new EmptyStackException();}
+			ele = Integer.parseInt(tempele);
+			}
+			catch (NumberFormatException io)
+			{JOptionPane.showMessageDialog(null, "Introduzca una opcion valida","ERROR",JOptionPane.WARNING_MESSAGE);}
+			catch (EmptyStackException io)//si el usuario apreta cancelar o quit sale del programa ya que devuelve un null
+			{ele=6;}
+			finally
+			{}
 			switch(ele)
 			{
 				case 1:
