107bc364f716a5d61b693eb3d3571af59c7539a4
tarowookjin <tarowj05@gmail.com>
2017-07-18 23:08:36 -0500

    Trabajando en la Logica

--- src/entities/solveINT.java
+++ src/entities/solveINT.java
@@ -8,13 +8,16 @@ public class solveINT {
 	String rango[];
 	int time=0;
 	interrupciones in = new interrupciones();
+	
 	public void solve(int finaltime){
 		time_faltante=new int[finaltime];
 		hayINT=new boolean[finaltime];
 		disp=new String[finaltime];
 		rango=new String[finaltime];
+		int priact=99;//prioridad actual
+		int i=0;
 		do {
-			
+		
 			
 		}while(time!=finaltime);
 	}
